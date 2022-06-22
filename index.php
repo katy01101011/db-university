ff8d5727c72a52b16d8b1bf53650f5659d18468b
katy01101011 <katy.91560@gmail.com>
2022-06-22 19:54:50 +0200

    stampa in html in modalità array

--- index.php
+++ index.php
@@ -1,29 +1,45 @@
 <?php
-    define('DB_SERVERNAME', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'root');
-    define('DB_NAME', 'university');
-    define('DB_PORT', 8889);
-    
-    $conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+require_once __DIR__ . "/server.php";
 
-    if ($conn && $conn->connect_error) {
-        echo "errore connessione al database" . $conn->connect_error;
-        die();
+// Imposto query per database
+$departmentsDB = "SELECT * FROM `departments`;";
+$result = $conn->query($departmentsDB);
+
+// Controllo che il risultato ci sia e che non sia vuoto
+if ($result && $result->num_rows > 0) {
+    // Mostro i risultati
+    while ($row = $result->fetch_assoc()) {
+        // var_dump($row);
     }
-
-    var_dump($conn);
+} elseif ($result && $result->num_rows = 0) {
+    // Query è corretta ma non ci sono risultati
+    echo "Non ci sono risultati";
+} else {
+    // Errore nella query
+    echo "Errore nella query";
+    die();
+};
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    ciao
+    <?php foreach ($result as $department) { ?>
+        <h2>
+            <?php
+                echo $department["name"];
+            ?>
+        </h2>
+        <a href="">Info dipartimento</a>
+    <?php } ?>
 </body>
+
 </html>
